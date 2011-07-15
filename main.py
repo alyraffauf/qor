25550d6d5708a9dfd19946313824222dface0d5b
Aly Raffauf <alyraffauf@gmail.com>
2011-07-14 20:29:07 -0400

    Added background, EnemyShip class, organized it better

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
 #!/usr/bin/env python
+# Alien Invasion: 2150 - a game inspired by Space Invaders
+# Copyright (C) 2011 Thomas Chace <ithomashc@gmail.com>
+
+# Alien Invasion: 2150 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Alien Invasion: 2150 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import pygame, sys, os
 from pygame.locals import *
 
-class AlienInvasion:
+def loadImage(name):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print("Cannot load image: %s" % name)
+        raise SystemExit, message
+    return image
+
+def loadSound(name):
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error, message:
+        print("Cannot load sound: %s" % name)
+        raise SystemExit, message
+    return sound
+
+
+class EnemyShip(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = loadImage("enemyship.bmp")
+        self.punching = 0
+
+class AlienInvasion():
     def __init__(self):
         pygame.init()
-        self.sound = self.loadSound("drill_down.ogg")
-        self.shipSurface = self.loadImage("spaceship.bmp")
+        self.sound = loadSound("drill_down.ogg")
+        self.shipSurface = loadImage("spaceship.bmp")
         self.window = pygame.display.set_mode((640, 480))
         pygame.display.set_caption("Alien Invasion: 2150")
         self.screen = pygame.display.get_surface()
+        
+        self.screen.blit(loadImage("background.bmp"), (0, 0))
+
+        pygame.display.flip()
         self.shipPosition = (320, 432)
         self.screen.blit(self.shipSurface, self.shipPosition)
-        print self.shipPosition[1]
+
         pygame.display.flip()
         self.sound.play()
-
-    def loadImage(self, name):
-        fullname = os.path.join('data', name)
-        try:
-            image = pygame.image.load(fullname)
-        except pygame.error, message:
-            print("Cannot load image: %s" % name)
-            raise SystemExit, message
-        return image
-
-    def loadSound(self, name):
-        fullname = os.path.join('data', name)
-        try:
-            sound = pygame.mixer.Sound(fullname)
-        except pygame.error, message:
-            print("Cannot load sound: %s" % name)
-            raise SystemExit, message
-        return sound
 
     def moveRight(self):
        self.shipPosition = (self.shipPosition[0] + 10, self.shipPosition[1])
@@ -55,6 +82,7 @@ class AlienInvasion:
             else: 
                 print(event)
 
-alienInvasion = AlienInvasion()
-while True:
-   alienInvasion.eventInput(pygame.event.get())
+if __name__ == '__main__':
+    alienInvasion = AlienInvasion()
+    while True:
+        alienInvasion.eventInput(pygame.event.get())
